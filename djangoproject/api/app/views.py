13289af47f584caeed8a5053d1528f8f2eac2eac
kamalkant255 <87160547+kamalkant255@users.noreply.github.com>
2024-06-25 22:13:27 +0530

    Add files via upload
    first commit

---
+++ djangoproject/api/app/views.py
@@ -0,0 +1,6 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+class HelloWorldViewSet(viewsets.ViewSet):
+    def list(self, request):
+        return Response({'message': 'Hello, world!'})
